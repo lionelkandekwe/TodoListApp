634b21d894ef3bf0d0fa527a30ff188067245d84
Lionel <liokandekwe@gmail.com>
2021-01-13 01:21:50 +0200

    Saving Todos to local Storage

--- script.js
+++ script.js
@@ -2,6 +2,11 @@ const form = document.getElementById('form')
 const input = document.getElementById('input')
 const TodosUL = document.getElementById('todos')
 
+const todos = JSON.parse(localStorage.getItem('todos'))
+if (todos) {
+  todos.forEach((todo) => addTodo(todo))
+}
+
 form.addEventListener('submit', (e) => {
   e.preventDefault()
   addTodo()
@@ -12,17 +17,34 @@ function addTodo(todo) {
     todoText = todo.text
   }
   if (todoText) {
-    let todoEl = document.createElement('li')
+    const todoEl = document.createElement('li')
     if (todo && todo.completed) {
       todoEl.classList.add('completed')
     }
     todoEl.innerText = todoText
-    todoEl.addEventListener('click', () => todoEl.classList.toggle('completed'))
+    todoEl.addEventListener('click', () => {
+      todoEl.classList.toggle('completed')
+      updateLS()
+    })
     todoEl.addEventListener('contextmenu', (e) => {
       e.preventDefault()
       todoEl.remove()
+      updateLS()
     })
     TodosUL.appendChild(todoEl)
     input.value = ''
+    updateLS()
   }
+}
+
+function updateLS() {
+  todoEl = document.querySelectorAll('li')
+  const todos = []
+  todoEl.forEach((todoEl) => {
+    todos.push({
+      text: todoEl.innerText,
+      completed: todoEl.classList.contains('completed'),
+    })
+  })
+  localStorage.setItem('todos', JSON.stringify(todos))
 }
